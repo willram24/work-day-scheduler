437e9a26327dcf5b305ca03e9c6e09b4d6c91d4d
willram24 <wramirez23@gmail.com>
2021-02-21 22:30:24 -0800

    date and day added

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Today's day and date
+var todayDate = moment().format('dddd, MMM Do, YYYY');
+$('#currentDay').html(todayDate);
+
+// click to save workday plan
+$(".saveBtn").on('click', => () {
+  
+
+}
